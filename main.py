1a3aa007a61e8d85a433d3cbb9bcfc822bd52f03
Alex-Vacool <vacoolenko@gmail.com>
2023-02-17 09:14:07 +0300

    Добавлена структура моделей данных с помощью SQLAlchemy

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from flask import Flask, render_template, request
 
 from hh_json import parce
+from crud import add_row
 
-# объявление главной переменной
 app = Flask(__name__)
 
 
-# вывод (редеринг) главной страницы
 @app.get('/index')
 @app.get('/')
 def index():
     return render_template('index.html')
 
 
-# вывод страницы формы
 @app.route('/form/')
 def form():
     return render_template('form.html')
@@ -21,12 +19,12 @@ def form():
 
 @app.post('/result/')
 def result():
-    """
-    Вывод результата обработки запроса
-    :return: страница с результатами
-    """
     vac = request.form
     data = parce(**vac)
-    dat = {**data, **vac}  # data | vac - в Python 3.10 можно сделать так
+    dat = {**data, **vac}  # data | vac
     print(dat)
+    dat['where'] = 'в названии вакансии' \
+        if dat['where'] == 'name' else 'в названии компании' if dat['where'] == 'company' else 'везде'
+    add_row(dat)
     return render_template('about.html', res=dat)
+
